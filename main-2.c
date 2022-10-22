5e563750093bd8284eab63b553e66f487e28f441
Alessio <99815094+AlessioS98@users.noreply.github.com>
2022-10-22 11:36:30 +0200

    Add files via upload

--- main-2.c
+++ main-2.c
@@ -1,158 +1,33 @@
 #include <stdio.h>
 #include <string.h>
-#define MAX_REMIND 50
-#define MSG_LEN 60
-
-void read_line(char str[], int n)
-{
-    char ch;
-    int i=0;
-    ch=getchar();
-    while(ch!='\n')
-    {
-        if(i<n)
-        {
-            str[i++]=ch;
-            ch=getchar();
-        }
-    }
-    str[i]='\0';
-}
+#include <stdlib.h>
 
 int main()
 {
-    char reminders[MAX_REMIND][MSG_LEN+3];
-    char day_str[3], hour_str[3], minute_str[3], mounth_str[3], msg_str[MSG_LEN+1];
-    int day, i, j, num_remind=0;
-    char mesegiorno[6], oraminuti[12], msn[73];
-    int mese, giorno, ore, minuti;
-    for(;;)
+    char argomento[50];
+    int i, j;
+    gets(argomento);
+    for(i=49; i>=7; i--)
     {
-        if(num_remind==MAX_REMIND)
+        if(*(argomento+i)=='\0' || *(argomento+i)!=' ')
         {
-            printf("--PROMEMORIA MASSIMI RAGGIUNTI--\n");
-            break;
+            continue;
         }else{
-            printf("Inserisci mese/giorno (mm/dd), orario (xx/yy) e il promemoria: ");
-            scanf("%d/%d %d:%d", &mese, &giorno, &ore, &minuti);
-            salto:
-            if(mese==0)
+            if(*(argomento+i)==' ')
             {
-                break;
-            }else{
-                while(mese<1 || mese>12 || giorno<0 || giorno>31)
+                for(j=i+1; ; j++)
                 {
-                    printf("Error: inserimento non valido!!\n");
-                    printf("Promemoria ignorato, riprovare\n");
-                    printf("Inserisci mese/giorno (mm/dd), orario (xx/yy) e il promemoria: ");
-                    scanf("%d/%d %d:%d", &mese, &giorno, &ore, &minuti);
-                    if(mese==0)
+                    if(*(argomento+j)=='\0' || *(argomento+j)==' ')
                     {
-                        goto salto;
-                    }
-                }
-                if(giorno>=1 && giorno<=9)
-                {
-                    sprintf(day_str, "%d", giorno);
-                    day_str[1]=day_str[0];
-                    day_str[0]='0';
-                }else{
-                    sprintf(day_str, "%d", giorno);
-                }
-                if(mese>=1 && mese<=9)
-                {
-                    sprintf(mounth_str, "%d", mese);
-                    mounth_str[1]=mounth_str[0];
-                    mounth_str[0]='0';
-                }else{
-                    sprintf(mounth_str, "%d", mese);
-                }
-                if(ore>=1 && ore<=9)
-                {
-                    sprintf(hour_str, "%d", ore);
-                    hour_str[1]=hour_str[0];
-                    hour_str[0]='0';
-                }else{
-                    sprintf(hour_str, "%d", ore);
-                }
-                if(minuti>=1 && minuti<=9)
-                {
-                    sprintf(minute_str, "%d", minuti);
-                    minute_str[1]=minute_str[0];
-                    minute_str[0]='0';
-                }else{
-                    sprintf(minute_str, "%d", minuti);
-                }
-                read_line(msg_str, MSG_LEN);
-                for(i=0; i<num_remind; i++)
-                {
-                    if(strcmp(mounth_str, reminders[i])<0)
-                    {
+                        printf(" ");
                         break;
                     }else{
-                        if(strcmp(mounth_str, reminders[i])==0)
-                        {
-                            strcpy(mesegiorno, mounth_str);
-                            strcat(mesegiorno, "/");
-                            strcat(mesegiorno, day_str);
-                            if(strcmp(mesegiorno, reminders[i])<0)
-                            {
-                                break;
-                            }else{
-                                if(strcmp(mesegiorno, reminders[i])==0)
-                                {
-                                    strcpy(oraminuti, mounth_str);
-                                    strcat(oraminuti, "/");
-                                    strcat(oraminuti, day_str);
-                                    strcat(oraminuti, " ");
-                                    strcat(oraminuti, hour_str);
-                                    strcat(oraminuti, ":");
-                                    strcat(oraminuti, minute_str);
-                                    if(strcmp(oraminuti, reminders[i])<0)
-                                    {
-                                        break;
-                                    }else{
-                                        if(strcmp(oraminuti, reminders[i])==0)
-                                        {
-                                            strcpy(msn, mounth_str);
-                                            strcat(msn, "/");
-                                            strcat(msn, day_str);
-                                            strcat(msn, " ");
-                                            strcat(msn, hour_str);
-                                            strcat(msn, ":");
-                                            strcat(msn, minute_str);
-                                            strcat(msn, msg_str);
-                                            if(strcmp(msn, reminders[i])<0)
-                                            {
-                                                break;
-                                            }
-                                        }
-                                    }
-                                }
-                            }
-                        }
+                        printf("%c", *(argomento+j));
                     }
                 }
-                for(j=num_remind; j>i; j--)
-                {
-                    strcpy(reminders[j], reminders[j-1]);
-                }
-                strcpy(reminders[i], mounth_str);
-                strcat(reminders[i], "/");
-                strcat(reminders[i], day_str);
-                strcat(reminders[i], " ");
-                strcat(reminders[i], hour_str);
-                strcat(reminders[i], ":");
-                strcat(reminders[i], minute_str);
-                strcat(reminders[i], msg_str);
-                num_remind++;
             }
         }
     }
-    printf("\nPromemoria inseriti\n");
-    for(i=0; i<num_remind; i++)
-    {
-        printf("%s\n", reminders[i]);
-    }
+    printf("\n");
     return 0;
 }
