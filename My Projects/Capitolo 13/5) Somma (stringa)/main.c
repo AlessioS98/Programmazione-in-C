3acd1c52567639c61d94b7093586b190302b00ce
Alessio <99815094+AlessioS98@users.noreply.github.com>
2022-10-26 17:37:17 +0200

    Add files via upload

---
+++ My Projects/Capitolo 13/5) Somma (stringa)/main.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main()
+{
+    char somma[50];
+    char addendo[10];
+    int i, j, k;
+    int numero, addizione;
+    addizione=0;
+    k=0;
+    gets(somma);
+    for(i=4; i<50; i++)
+    {
+        if(somma[i]=='\0')
+        {
+            break;
+        }else{
+            if(somma[i]!=' ')
+            {
+                addendo[k++]=somma[i];
+            }else{
+                numero=atoi(addendo);
+                addizione=addizione+numero;
+                for(j=0; j<10; j++)
+                {
+                    addendo[j]='\0';
+                }
+                k=0;
+            }
+        }
+    }
+    printf("La somma totale e': %d\n", addizione);
+    return 0;
+}
