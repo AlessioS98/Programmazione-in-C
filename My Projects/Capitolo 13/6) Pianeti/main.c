527002cddf9ab49e60783bc844e5a07ed92832cb
Alessio <99815094+AlessioS98@users.noreply.github.com>
2022-10-30 02:07:05 +0200

    Add files via upload

--- My Projects/Capitolo 13/6) Pianeti/main.c
+++ My Projects/Capitolo 13/6) Pianeti/main.c
@@ -5,15 +5,26 @@
 
 int main(int argc, char *argv[])
 {
+    int lenght;
     char *planets[]={"Mercurio", "Venere", "Terra",
                      "Marte", "Giove", "Saturno",
-                     "Urano", "Nettuno", "Plutone",
-                     "mercurio", "venere", "terra",
-                     "marte", "giove", "saturno",
-                     "urano", "nettuno", "plutone"};
-    int i, j;
+                     "Urano", "Nettuno", "Plutone"};
+    int i, j, k;
     for(i=1; i<argc; i++)
     {
+        lenght=strlen(argv[i]);
+        for(k=0; k<lenght; k++)
+        {
+            if(k==0)
+            {
+                if(*argv[i]>='a' && *argv[i]<='z')
+                {
+                    *argv[i]=*(argv[i])-32;
+                }
+            }else{
+                *(argv[i]+k)=tolower(*(argv[i]+k));
+            }
+        }
         for(j=0; j<NUM_PLANETS; j++)
         {
             if(strcmp(argv[i], planets[j])==0)
