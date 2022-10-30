24df3c581a272934d9703be5173b215d29e25676
Alessio <99815094+AlessioS98@users.noreply.github.com>
2022-10-30 21:12:06 +0100

    Add files via upload

---
+++ main.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int compute_vowel_count(const char *sentence)
+{
+    int cont;
+    int i;
+    for(i=0; (*sentence+i)!='\0'; i++)
+    {
+        if((*sentence+i)=='A' || (*sentence+i)=='E' || (*sentence+i)=='I' || (*sentence+i)=='O' || (*sentence+i)=='U' || (*sentence+i)=='a' || (*sentence+i)=='e' || (*sentence+i)=='i' || (*sentence+i)=='o' || (*sentence+i)=='u')
+        {
+            ++cont;
+        }else{
+            continue;
+        }
+    }
+    return cont;
+}
+
+int main()
+{
+    char stringa[50];
+    int vocali;
+    printf("Inserisci una frase: ");
+    gets(stringa);
+    vocali=compute_vowel_count(stringa);
+    printf("Le vocali all'interno della frase sono %d", vocali);
+    return 0;
+}
