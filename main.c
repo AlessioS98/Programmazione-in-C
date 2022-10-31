44d8be1fb0818f5f3deef5a84025d7ebbb600d2d
Alessio <99815094+AlessioS98@users.noreply.github.com>
2022-10-31 10:27:35 +0100

    Update main.c

--- main.c
+++ main.c
@@ -6,11 +6,15 @@ int compute_vowel_count(const char *sentence)
 {
     int cont;
     int i;
-    for(i=0; (*sentence+i)!='\0'; i++)
+    cont=0;
+    for(i=0; *(sentence+i)!='\0'; i++)
     {
-        if((*sentence+i)=='A' || (*sentence+i)=='E' || (*sentence+i)=='I' || (*sentence+i)=='O' || (*sentence+i)=='U' || (*sentence+i)=='a' || (*sentence+i)=='e' || (*sentence+i)=='i' || (*sentence+i)=='o' || (*sentence+i)=='u')
+        if(*(sentence+i)=='A' || *(sentence+i)=='E' || *(sentence+i)=='I' 
+        || *(sentence+i)=='O' || *(sentence+i)=='U' 
+        || *(sentence+i)=='a' || *(sentence+i)=='e' || *(sentence+i)=='i' 
+        || *(sentence+i)=='o' || *(sentence+i)=='u')
         {
-            ++cont;
+            cont++;
         }else{
             continue;
         }
