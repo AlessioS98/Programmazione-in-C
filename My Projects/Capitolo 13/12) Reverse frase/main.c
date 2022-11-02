c3f6560f53143ded92455461b263e80b1cf11faf
Alessio <99815094+AlessioS98@users.noreply.github.com>
2022-11-02 16:58:06 +0100

    Add files via upload

---
+++ My Projects/Capitolo 13/12) Reverse frase/main.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main()
+{
+    char parole[30][20];
+    char frase[80];
+    char terminazione;
+    int i, j, k, rim, num, pros;
+    int cont;
+    printf("Inserisci una frase: ");
+    gets(frase);
+    k=0;
+    j=0;
+    cont=0;
+    for(i=0; i<80; i++)
+    {
+        if(frase[i]=='?' || frase[i]=='!' || frase[i]=='.')
+        {
+            terminazione=frase[i];
+        }else{
+            if(frase[i]!=' ')
+            {
+                parole[k][j]=frase[i];
+                j++;
+            }else{
+                for(rim=j; rim<20; rim++)
+                {
+                    parole[k][rim]='\0';
+                }
+                k++;
+                cont++;
+                j=0;
+            }
+        }
+    }
+    num=strlen(parole[cont]);
+    for(pros=num; pros<20; pros++)
+    {
+        parole[cont][pros]='\0';
+    }
+    printf("Frase in reverse: ");
+    for(k=cont; k>=0; k--)
+    {
+        printf("%s", parole[k]);
+        if(k==0)
+        {
+            if(terminazione=='?' || terminazione=='!' || terminazione=='.')
+            {
+                printf("%c", terminazione);
+            }
+        }
+        printf(" ");
+    }
+    printf("\n");
+    return 0;
+}
