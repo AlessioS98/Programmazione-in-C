d620d237345f527e1d9bd5e437648e3505e4138f
Alessio <99815094+AlessioS98@users.noreply.github.com>
2022-12-23 11:45:47 +0100

    Add files via upload

---
+++ My Projects/Capitolo 16/4) Inventario - piccola modifica/inventory.c
@@ -0,0 +1,153 @@
+#include <stdio.h>
+#include "readline.h"
+#define NAME_LEN 25
+#define MAX_PARTS 100
+
+struct part{
+    int number;
+    char name[NAME_LEN+1];
+    int on_hand;
+    float price;
+}inventory[MAX_PARTS];
+int num_parts=0;
+int find_part(int number);
+void insert(void);
+void search(void);
+void update(void);
+void print(void);
+
+int main()
+{
+    char code;
+    for(;;)
+    {
+        printf("Inserisci un codice operazione: ");
+        scanf("%c", &code);
+        switch(code)
+        {
+            case 'i': insert();
+                      break;
+            case 's': search();
+                      break;
+            case 'u': update();
+                      break;
+            case 'p': print();
+                      break;
+            case 'q': return 0;
+            default: printf("Codice inesistente\n");
+        }
+        printf("\n");
+    }
+}
+
+int find_part(int number)
+{
+    int i;
+    for(i=0; i<num_parts; i++)
+    {
+        if(inventory[i].number==number)
+        {
+            return i;
+        }
+    }
+    return -1;
+}
+
+void insert(void)
+{
+    int part_number;
+    if(num_parts==MAX_PARTS)
+    {
+        printf("Il Database e' pieno, non puoi aggiungere altri componenti.\n");
+        return;
+    }
+    printf("Inserisci numero componente: ");
+    scanf("%d", &part_number);
+    if(find_part(part_number)>=0)
+    {
+        printf("Componente gia' esistente.\n");
+        return;
+    }
+    inventory[num_parts].number=part_number;
+    printf("Inserisci il nome del componente: ");
+    read_line(inventory[num_parts].name, NAME_LEN);
+    printf("Inserisci quantita' componente: ");
+    scanf("%d", &inventory[num_parts].on_hand);
+    printf("Inserisci il prezzo del componente: €");
+    scanf("%f", &inventory[num_parts].price);
+    num_parts++;
+}
+
+void search(void)
+{
+    int i, number;
+    printf("Inserisci numero componente: ");
+    scanf("%d", &number);
+    i=find_part(number);
+    if(i>=0)
+    {
+        printf("Nome componente: %s\n", inventory[i].name);
+        printf("Quantita' componente: %d\n", inventory[i].on_hand);
+        printf("Prezzo ocmponente: %.2f\n", inventory[i].price);
+    }else{
+        printf("Componente non trovato.\n");
+    }
+}
+
+void update(void)
+{
+    int i, number, change;
+    float changeprice;
+    printf("Inserisci numero componente: ");
+    scanf("%d", &number);
+    i=find_part(number);
+    if(i>=0)
+    {
+        printf("Aggiungi quantita': ");
+        scanf("%d", &change);
+        printf("Aggiungi prezzo: ");
+        scanf("%f", &changeprice);
+        inventory[i].price+=changeprice;
+        inventory[i].on_hand+=change;
+    }else{
+        printf("Componente non trovato.\n");
+    }
+}
+
+void print(void)
+{
+    struct part supporto;
+    int minimo;
+    int i, j, k, indicemin;
+    printf("Numero componente     Nome componente     Quantita' componente     Prezzo componente\n");
+    j=0;
+    while(j<num_parts)
+    {
+        minimo=inventory[j].number;
+        supporto=inventory[j];
+        for(i=j; i<num_parts; i++)
+        {
+            if(inventory[i].number<minimo)
+            {
+                minimo=inventory[i].number;
+            }else{
+                continue;
+            }
+        }
+        for(k=j; k<num_parts; k++)
+        {
+            if(minimo==inventory[k].number)
+            {
+                indicemin=k;
+                break;
+            }
+        }
+        inventory[j]=inventory[indicemin];
+        inventory[indicemin]=supporto;
+        j++;
+    }
+    for(i=0; i<num_parts; i++)
+    {
+        printf("%7d       %-25s%11d                 %.2f\n", inventory[i].number, inventory[i].name, inventory[i].on_hand, inventory[i].price);
+    }
+}
